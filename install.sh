379482a877fde04b87e6f24ad3e7ff28b83b0d05
bperraud <74benji@live.fr>
2024-09-29 21:41:37 +0200

    ignore file with stow

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ sudo apt update
 sudo apt install python3-venv
 sudo apt install npm
 sudo apt install unzip
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
