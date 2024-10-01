600a5a1cca310137b0dea262648ed5defef6f689
bperraud <74benji@live.fr>
2024-10-01 12:38:27 +0200

    nvim-tree

--- .config/nvim/lua/bperraud/packer.lua
+++ .config/nvim/lua/bperraud/packer.lua
@@ -24,6 +24,13 @@ return require('packer').startup(function(use)
 	  --end
   --})
 
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+}
+
   use { "catppuccin/nvim", as = "catppuccin" }
 
   -- comment
--- .config/nvim/lua/bperraud/remap.lua
+++ .config/nvim/lua/bperraud/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -11,6 +10,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set('v', '<C-_>', ':Commentary<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-_>', ':Commentary<CR>', { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
@@ -44,12 +44,30 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- vim.keymap.set(
+--     "n",
+--     "<leader>ee",
+--     "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+-- )
+
+local function toggle_nvim_tree_without_focus()
+    local current_window_id = vim.api.nvim_get_current_win()
+    vim.cmd('NvimTreeToggle')
+    vim.api.nvim_set_current_win(current_window_id)
+end
+
+--vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set(
     "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+    "<leader>e",
+    ":NvimTreeFocus<CR>", { silent = true }
 )
 
+vim.keymap.set(
+    "n",
+    "<C-n>",
+    toggle_nvim_tree_without_focus, { silent = true }
+)
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
