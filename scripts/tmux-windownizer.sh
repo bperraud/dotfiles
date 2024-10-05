73dfc9ce0a59ccbacde73449807942aa5cb7a54d
bperraud <74benji@live.fr>
2024-10-05 17:02:37 +0200

    tmux windownizer correct window id

--- scripts/tmux-windownizer.sh
+++ scripts/tmux-windownizer.sh
@@ -13,6 +13,9 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
+# kill active window
+# tmux kill-window -t $(tmux list-windows -F '#{window_id}' | sort -n | tail -n 1)
+
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
     tmux new-session -s $selected_name -c $selected
     exit 0
@@ -21,5 +24,9 @@ fi
 # Create a new window in the current session
 tmux neww -n "$selected_name" -c "$selected"
 
+new_window_index=$(tmux display-message -p '#I')
+
 # Switch to the newly created window
 tmux select-window -t "$selected_name"
+
+tmux swap-window -s $((new_window_index - 1)) -t "$new_window_index"
