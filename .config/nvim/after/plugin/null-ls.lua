710fd2a28e31f9702facd73d26e193a06f6664be
bperraud <74benji@live.fr>
2024-10-17 23:55:57 +0200

    black format on save

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -8,17 +8,17 @@ null_ls.setup({
     },
 
     on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            -- Create an autocmd that formats the buffer on save
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format()
+				end,
+			})
+		end
+	end,
 })
 
 --[[
