cb49c3c0831071c52a874d174706993724bdb932
bperraud <74benji@live.fr>
2024-12-13 20:22:15 +0100

    lsp keymaps

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -22,7 +22,13 @@ lsp.set_preferences({
 
 lsp.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false}
-    attach(opts)
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    -- attach(opts)
 end)
 
 lsp.preset("recommended")
@@ -89,10 +95,6 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
-    attach(opts)
-end)
 
 lsp.setup()
 
