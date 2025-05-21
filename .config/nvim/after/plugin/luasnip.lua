a1638ccf6278e9f02f5b46abfd705b487b50d9fb
bperraud <74benji@live.fr>
2025-05-21 17:09:59 +0200

    nvim-treesiter setup

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,4 +1,3 @@
--- local cmp = require("cmp")
 local ls = require("luasnip")
 
 vim.keymap.set({"i"}, "<C-i>", function() ls.expand() end, {silent = true})
@@ -6,7 +5,6 @@ vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
 
 require("luasnip.loaders.from_vscode").lazy_load()
-
 
 ls.config.setup({
     enable_autosnippets = true,
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,20 +1,16 @@
+-- Setup devicons (optional)
+require('nvim-web-devicons').setup {
+  default = true, -- Ensures that default icons are used
+}
+
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "html"},
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  ensure_installed = { "lua", "python", "html", "javascript", "typescript", "json", "bash", "yaml", "c", "vim", "cpp", "dockerfile", "go", "helm", "htmldjango", "nginx", "php", "rust"},
   auto_install = true,
-
   highlight = {
     enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
-  }}
+  },
+  indent = {
+    enable = true,
+  },
+}
